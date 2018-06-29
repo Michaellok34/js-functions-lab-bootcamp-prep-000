6d75a349226b4da356e8b030ef479df0d9702544
Michael Lok <michaellok34@yahoo.co.uk>
2018-06-29 17:18:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function happyHolidays(){
 
 
 
-function happyHolidaysTo(name) {
-  return(`Happy holidays, ${name}!`)
+function happyHolidaysTo(holiday,name) {
+  return(`Happy ${holidays}, ${name}!`)
 }
